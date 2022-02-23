71f0ae4376ac702a5eb7be0ce6957d647e1847a0
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-23 18:21:14 +0530

    [Guru] Welcome to Indian state census analyzer program

---
+++ IndianStateCensusAnalyzerProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyzerProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyzer Program");
+
+            Console.ReadLine();
+        }
+    }
+}
